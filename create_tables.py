6cfdd351ed0e4e0c023d0638bca9e7b2bfe191ba
spnichol <info@truediggs.com>
2017-04-09 10:48:21 -0400

    added postgres

---
+++ create_tables.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  9 10:06:04 2017
+
+@author: spnichol
+"""
+import psycopg2
+conn_string = "host='justnew.cl95re7xujec.us-east-1.rds.amazonaws.com' dbname='postgres' user='pres' password='marigold'"
+print "Connecting to database\n	->%s" % (conn_string)
+ 
+conn = psycopg2.connect(conn_string)
+ 
+cursor = conn.cursor()
+sql = "CREATE TABLE vid_list (youid varchar(25), title varchar(500), pubdate date, query varchar(500), chanid varchar(200), lat varchar(100), lon varchar(100), geoquery varchar(200), radius varchar(200) );"
+cursor.execute(sql)
+conn.commit()
+
